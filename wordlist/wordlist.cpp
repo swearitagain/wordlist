5d624a3acd00a36bde8a78650d63ee36b4f183f0
swearitagain <37062654+swearitagain@users.noreply.github.com>
2019-03-06 17:37:12 +0800

    封装了计算模块
    现在core.h中包含了标准要求的三种api

--- wordlist/wordlist.cpp
+++ wordlist/wordlist.cpp
@@ -3,31 +3,57 @@
 
 #include "pch.h"
 #include <iostream>
-//#include "calculateInterface.h"
 #include "calculate.h"
 #include "specified_calculate.h"
 #include "circle_calculate.h"
+#include <string>
+#include "core.h"
+
 int main()
 {
     std::cout << "Hello World!\n";
 
-	calculateInterface *cal = new calculate({ "half", "pig", "giggle", "fun", "none" }, false);
-	vector<string> *result = cal->get_result();
+	char a[] = "ab";
+	char b[] = "bv";
+	char *words[] = {a,b};
 
-	delete cal;
+	int length = 2;
+	char * result[2] = { b,a };
+	vector<string> *result_v = nullptr;
+	core *my_core = new core();
+	try {
+		my_core->gen_chain(words, length, result);
+		result_v = my_core->gen_chain_word_v({ "half", "pig", "giggle", "fun", "none" }, 0, 0, false);
+	}catch (exception e)
+	{
+		cout << e.what() << endl;
+	}
+	for(int i = 0; i < length; i++)
+	{
+		cout << result[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0 ; i < result_v->size();i++)
+	{
+		cout << result_v->at(i) << " ";
+	}
+	//calculateInterface *cal = new calculate({ "half", "pig", "giggle", "fun", "none" }, false);
+	//vector<string> *result = cal->get_result();
 
-	cal = new specified_calculate({ "half", "pig", "giggle", "fun", "none" }, false, ALPHA_TO_INDEX('p'), -1);
-	result = cal->get_result();
+	//delete cal;
 
-	delete cal;
+	//cal = new specified_calculate({ "half", "pig", "giggle", "fun", "none" }, false, ALPHA_TO_INDEX('p'), -1);
+	//result = cal->get_result();
 
-	cal = new specified_calculate({ "half", "pig", "giggle", "fun", "niupi" }, false, -1, ALPHA_TO_INDEX('e'));
-	result = cal->get_result();
+	//delete cal;
 
-	delete cal;
+	//cal = new specified_calculate({ "half", "pig", "giggle", "fun", "niupi" }, false, -1, ALPHA_TO_INDEX('e'));
+	//result = cal->get_result();
 
-	cal = new circle_calculate({ "half", "pig", "gap", "fun", "neo", "op" }, false, -1, -1, true);
-	result = cal->get_result();
+	//delete cal;
+
+	//cal = new circle_calculate({ "half", "pig", "gap", "fun", "neo", "op" }, false, -1, -1, true);
+	//result = cal->get_result();
 
 }
 
